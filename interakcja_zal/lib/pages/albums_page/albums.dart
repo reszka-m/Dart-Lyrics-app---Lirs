572ad98b795c0cc58511de4b75bcd5ad5e18ce86
reszka-m <reszkam19@gmail.com>
2021-03-30 13:40:53 +0200

    :)

--- interakcja_zal/lib/pages/albums_page/albums.dart
+++ interakcja_zal/lib/pages/albums_page/albums.dart
@@ -5,8 +5,6 @@ import 'package:interakcja_zal/models/albums.dart';
 import 'package:interakcja_zal/services/api-manager.dart';
 import 'package:interakcja_zal/constants.dart';
 
-
-
 class Pizda extends StatefulWidget {
   final int artistId;
   final String artistNAME;
@@ -37,89 +35,99 @@ class _PizdaState extends State<Pizda> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: new AppBar(
-        automaticallyImplyLeading: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            SizedBox(
-              child: Logo(),
-            ),
-            SizedBox(
-              child: Text(
-                "Albumy wykonawcy:\n" + artistNAME,
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w700,
-                  color: kSecondaryColor,
+      body: Column(
+        children: [
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(
+                  child: Logo(),
                 ),
-                textAlign: TextAlign.center,
-              ),
-              width: 250,
+              ],
             ),
-          ],
-        ),
-      ),
-      body: Container(
-        child: FutureBuilder<Albums>(
-          future: _albums,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.message.body.albumList.length,
-                itemBuilder: (context, index) {
-                  var albums = snapshot.data.message.body.albumList[index];
-
-                  String datastring = DateFormat('yyyy-MM-dd')
-                      .format(albums.album.albumReleaseDate);
-                  return Container(
-                    padding: EdgeInsets.all(15.0),
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(color: Colors.black54, width: 4.0),
-                      ),
-                    ),
-                    child: Row(
-                      children: <Widget>[
-                        SizedBox(
-                          width: 100,
-                          child: Text(
-                            "Data wydania: " + "\n" + datastring,
-                            style: TextStyle(
-                                color: Colors.white.withOpacity(0.59),
-                                fontFamily: 'Poppins',
-                                fontWeight: FontWeight.w300,
-                                fontSize: 12),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+                left: kDefaultPadding * 0.5,
+                right: kDefaultPadding * 0.5,
+                top: kDefaultPadding),
+            color: Colors.red.withOpacity(0.5),
+            child: Text(
+              "Albumy wykonawcy:\n" + artistNAME,
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w700,
+                color: kSecondaryColor,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            width: size.width * 0.5,
+            height: size.height * 0.1,
+          ),
+          Container(
+            height: size.height * 0.8,
+            child: FutureBuilder<Albums>(
+              future: _albums,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    itemCount: snapshot.data.message.body.albumList.length,
+                    itemBuilder: (context, index) {
+                      var albums = snapshot.data.message.body.albumList[index];
+                      String datastring = DateFormat('yyyy-MM-dd')
+                          .format(albums.album.albumReleaseDate);
+                      return Container(
+                        padding: EdgeInsets.all(15.0),
+                        decoration: BoxDecoration(
+                          border: Border(
+                            bottom:
+                                BorderSide(color: Colors.black54, width: 4.0),
                           ),
                         ),
-                        Flexible(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Text(
-                                albums.album.albumName,
+                        child: Row(
+                          children: <Widget>[
+                            SizedBox(
+                              width: 100,
+                              child: Text(
+                                "Data wydania: " + "\n" + datastring,
                                 style: TextStyle(
-                                  color: Colors.white.withOpacity(0.8),
-                                  fontSize: 16,
-                                  fontFamily: 'Poppins',
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                textAlign: TextAlign.center,
+                                    color: Colors.white.withOpacity(0.59),
+                                    fontFamily: 'Poppins',
+                                    fontWeight: FontWeight.w300,
+                                    fontSize: 12),
                               ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
+                            ),
+                            Flexible(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.stretch,
+                                children: <Widget>[
+                                  Text(
+                                    albums.album.albumName,
+                                    style: TextStyle(
+                                      color: Colors.white.withOpacity(0.8),
+                                      fontSize: 16,
+                                      fontFamily: 'Poppins',
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
                   );
-                },
-              );
-            } else
-              return CircularProgressIndicator();
-          },
-        ),
+                } else
+                  return CircularProgressIndicator();
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
