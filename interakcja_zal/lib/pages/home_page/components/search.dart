afbd5b43c5cda47d8d042191def3838464ba7b6c
Mateusz Szreder <mateusz.szreder00Gmail.com>
2021-04-23 21:50:05 +0200

    kueqXHGGYH

--- interakcja_zal/lib/pages/home_page/components/search.dart
+++ interakcja_zal/lib/pages/home_page/components/search.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:interakcja_zal/pages/results_page/tracksresults.dart';
 import '../../../app_icons.dart';
 
 class Search extends StatelessWidget {
@@ -61,9 +62,6 @@ class DataSearch extends SearchDelegate<String> {
   }
 
   @override
-  Widget buildResults(BuildContext context) {}
-
-  @override
   Widget buildSuggestions(BuildContext context) {
     final recent = query.isEmpty ? recentsong : searchsongs;
 
@@ -75,6 +73,32 @@ class DataSearch extends SearchDelegate<String> {
         title: Text(searchsongs[index]),
       ),
       itemCount: searchsongs.length,
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    if (query.length < 1) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+            child: Text(
+              "Dokładna nazwa piosenki lub artysty",
+            ),
+          )
+        ],
+      );
+    }
+
+    var results = query;
+    print(results);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Tracksresults(trackNAME: results),
+      ),
     );
   }
 }
--- interakcja_zal/lib/pages/home_page/home_page.dart
+++ interakcja_zal/lib/pages/home_page/home_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:interakcja_zal/pages/albums_page/albums.dart';
 import 'package:interakcja_zal/pages/home_page/body1.dart';
 import 'package:interakcja_zal/pages/home_page/components/search.dart';
+import 'package:interakcja_zal/pages/results_page/tracksresults.dart';
 
 class HomePage extends StatelessWidget {
   static String routeName = '/home';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Body1(),
+      body: Tracksresults(),
     );
   }
 }
